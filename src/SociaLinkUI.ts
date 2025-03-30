2159277a8ae2bbc647746ddbd8b6b62a214dca09
GGMachina <git@ggsource.dev>
2025-03-30 12:37:28 -0500

    Improved link ui section layouts w/ margins and gaps

--- src/SociaLinkUI.ts
+++ src/SociaLinkUI.ts
@@ -16,15 +16,20 @@ export class SocialLinkUI {
         let linkUILeft = document.createElement("div");
         linkUILeft.className = "linkSection linkLeft";
         linkUILeft.innerHTML = "Area for the arcana card";
+        linkUILeft.style.flex = "1"; // Make the card take up 1 fraction of the space
         // TODO: Turn linkleft into the full card using multiple divs
         let linkCenter = document.createElement("div");
         linkCenter.className = "linkSection linkCenter";
         linkCenter.innerHTML = "Area for the level and arcana name";
+        linkCenter.style.flex = "3"; // fractions of the space
         // TODO: Insert the rank and the arcana below it inside linkCenter
         let linkUIRight = document.createElement("div");
         linkUIRight.className = "linkSection linkRight";
         linkUIRight.innerHTML = "Area for the bar and the name of the social link";
+        linkUIRight.style.flex = "6"; //fractions of the space
         // TODO: Insert the pip bar and the name of the social link inside linkRight
+
+        // TODO: Figure out how the color of the line between each link row is decided in original
 
         // Attach all three sections to the full card
         linkUIFull.appendChild(linkUILeft);
